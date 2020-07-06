74e23e12619218f01ae310574b88cfa2f95fb21d
mikedane <mikedane94@gmail.com>
2020-07-07 06:58:38 +1000

    the solution code

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Autocomplete = ({possibleSearchResults, onSelect}) => {
+const Autocomplete = ({possibleSearchResults}) => {
+
+    const [searchValue, setSearchValue] = useState("")
+
     return (
         <div>
-            
+            <input
+                value={searchValue}
+                onChange={e => setSearchValue(e.target.value)}
+            />
+            {searchValue && possibleSearchResults.filter(r => r.includes(searchValue)).map(t => <li>{t}</li>)}
         </div>
     )
 }
